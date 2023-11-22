6e3b289836ea392491a2ad4ce675db7d11cb8645
uenoaki <radaokuno2525@gmail.com>
2023-11-23 06:19:48 +0900

    undo_with_previous

--- src/command/mod.rs
+++ src/command/mod.rs
@@ -1,23 +1,17 @@
 use anyhow::{anyhow, Result};
 
 pub trait Command<Target> {
-    fn execute(&self, target: &mut Target);
-    fn undo(&self,target:&mut Target);
+    fn execute(&mut self, target: &mut Target);
+    fn undo(&mut self, target: &mut Target);
 }
 
-pub struct CommandManager<'a, Cmd, Target>
-where
-    Cmd: Command<Target>,
-{
-    queue_stack: Vec<Cmd>,
+pub struct CommandManager<'a, Target: 'a> {
+    queue_stack: Vec<Box<dyn Command<Target> + 'a>>,
     target: &'a mut Target,
     index: usize,
 }
 
-impl<'a, Cmd, Target> CommandManager<'a, Cmd, Target>
-where
-    Cmd: Command<Target>,
-{
+impl<'a, Target> CommandManager<'a, Target> {
     pub fn new(target: &'a mut Target) -> Self {
         CommandManager {
             queue_stack: Vec::new(),
@@ -33,10 +27,9 @@ where
         if self.queue_stack.len() <= self.index {
             Err(anyhow!("Done"))
         } else {
-            let command = &self.queue_stack[self.index];
-            let target = &mut self.target;
+            let cmd = &mut self.queue_stack[self.index];
+            cmd.execute(&mut self.target);
             self.index += 1;
-            command.execute(target);
             Ok(())
         }
     }
@@ -44,22 +37,21 @@ where
         loop {
             if let Err(_) = self.execute() {
                 break;
-            }    
+            }
         }
     }
     pub fn undo(&mut self) -> Result<()> {
         if 0 == self.index {
-            return Err(anyhow!("Cannot go back"))
+            return Err(anyhow!("Couldn't go back"));
         } else {
             self.index -= 1;
-            let c = &self.queue_stack[self.index];
-            let t = &mut *self.target;
-            c.undo(t);
+            let cmd = &mut self.queue_stack[self.index];
+            cmd.undo(&mut self.target);
             Ok(())
         }
     }
-    pub fn append(&mut self, command: Cmd) {
-        self.queue_stack.push(command);
+    pub fn append<Cmd: Command<Target> + 'a>(&mut self, command: Cmd) {
+        self.queue_stack.push(Box::new(command));
     }
     pub fn clear(&mut self) {
         self.queue_stack.clear();
@@ -72,69 +64,119 @@ mod test {
     use super::*;
     #[derive(Debug, PartialEq)]
     struct Actor {
-        x:i64,
-        y:i64,
-        dx:i64,
-        dy:i64
+        name: &'static str,
+        x: i64,
+        y: i64,
+        dx: i64,
+        dy: i64,
     }
     impl Actor {
-        pub fn new() -> Self{
-            Actor {x: 0, y: 0, dx: 1, dy: 0 }
+        pub fn new() -> Self {
+            Actor {
+                name: "",
+                x: 0,
+                y: 0,
+                dx: 1,
+                dy: 0,
+            }
         }
         pub fn go_forward(&mut self) {
             self.x += self.dx;
             self.y += self.dy;
         }
-        pub fn set_velocity(&mut self,v:(i64,i64)) {
+        pub fn set_velocity(&mut self, v: (i64, i64)) {
             self.dx = v.0;
             self.dy = v.1;
         }
-        pub fn get_velocity(&mut self) -> (i64,i64){
-            (self.dx,self.dy)
+        pub fn get_velocity(&mut self) -> (i64, i64) {
+            (self.dx, self.dy)
         }
     }
-    enum ActorCommand {
-        GoForward,
-        TurnRight,
-        TurnLeft,
+
+    struct GoForward;
+    impl Command<Actor> for GoForward {
+        fn execute(&mut self, target: &mut Actor) {
+            target.go_forward();
+        }
+        fn undo(&mut self, target: &mut Actor) {
+            let mut cmd = TurnRight;
+            cmd.execute(target);
+            cmd.execute(target);
+            self.execute(target);
+            cmd.execute(target);
+            cmd.execute(target);
+        }
     }
-    impl Command<Actor> for ActorCommand {
-        fn execute(&self, target: &mut Actor) {
-            use ActorCommand::*;
-            match self {
-                GoForward => {
-                    target.go_forward();
-                }
-                TurnRight => {
-                    let (dx,dy) = target.get_velocity();
-                    target.set_velocity((dy,-dx));
-                },
-                TurnLeft => {
-                    let (dx,dy) = target.get_velocity();
-                    target.set_velocity((-dy,dx));
-                }
+    struct TurnRight;
+    impl Command<Actor> for TurnRight {
+        fn execute(&mut self, target: &mut Actor) {
+            let (dx, dy) = target.get_velocity();
+            target.set_velocity((dy, -dx));
+        }
+        fn undo(&mut self, target: &mut Actor) {
+            let mut cmd = TurnLeft;
+            cmd.execute(target);
+        }
+    }
+    struct TurnLeft;
+    impl Command<Actor> for TurnLeft {
+        fn execute(&mut self, target: &mut Actor) {
+            let (dx, dy) = target.get_velocity();
+            target.set_velocity((-dy, dx));
+        }
+        fn undo(&mut self, target: &mut Actor) {
+            let mut cmd = TurnRight;
+            cmd.execute(target);
+        }
+    }
+    struct Teleport {
+        pub position: (i64, i64),
+        prev: Option<(i64, i64)>,
+    }
+    impl Teleport {
+        pub fn new(position: (i64, i64)) -> Self {
+            Self {
+                position,
+                prev: None,
             }
         }
-        fn undo(&self,target:&mut Actor) {
-            use ActorCommand::*;
-            match self {
-                GoForward => {
-                    let cmd = TurnRight;
-                    cmd.execute(target);
-                    cmd.execute(target);
-                    self.execute(target);
-                    cmd.execute(target);
-                    cmd.execute(target);
-                }
-                TurnRight => {
-                    let cmd = TurnLeft;
-                    cmd.execute(target);
-                },
-                TurnLeft => {
-                    let cmd = TurnRight;
-                    cmd.execute(target);
-                }
-            }
+    }
+    impl Command<Actor> for Teleport {
+        fn execute(&mut self, target: &mut Actor) {
+            self.prev = Some((target.x, target.y));
+            target.x = self.position.0;
+            target.y = self.position.1;
+        }
+        fn undo(&mut self, target: &mut Actor) {
+            let pos = match self.prev {
+                Some(v) => v,
+                None => (0, 0),
+            };
+            let mut cmd = Teleport::new(pos);
+            cmd.execute(target);
+        }
+    }
+    struct Rename {
+        pub name: &'static str,
+        prev: Option<&'static str>,
+    }
+    impl Rename {
+        pub fn new(name: &'static str) -> Self {
+            Self { name, prev: None }
+        }
+    }
+    impl Command<Actor> for Rename {
+        fn execute(&mut self, target: &mut Actor) {
+            self.prev = Some(target.name);
+            target.name = self.name;
+        }
+        fn undo(&mut self, target: &mut Actor) {
+            let name = match self.prev {
+                Some(v) => v,
+                None => "",
+            };
+            let mut cmd = Rename::new(name);
+            cmd.execute(target);
         }
     }
     #[test]
@@ -142,8 +184,9 @@ mod test {
         let mut actor = Actor::new();
         let mut invoker = CommandManager::new(&mut actor);
         {
-            use ActorCommand::*;
+            invoker.append(Rename::new("Tom"));
             invoker.append(GoForward);
+            invoker.append(Teleport::new((10, 10)));
             invoker.append(TurnRight);
             invoker.append(GoForward);
         }
@@ -151,30 +194,33 @@ mod test {
         assert_eq!(
             *invoker.target,
             Actor {
-                x:1,
-                y:0,
-                dx:1,
-                dy:0
+                name: "Tom",
+                x: 0,
+                y: 0,
+                dx: 1,
+                dy: 0
             }
         );
         invoker.execute_all();
         assert_eq!(
             *invoker.target,
             Actor {
-                x:1,
-                y:-1,
-                dx:0,
-                dy:-1
+                name: "Tom",
+                x: 10,
+                y: 9,
+                dx: 0,
+                dy: -1
             }
         );
         invoker.undo().unwrap();
         assert_eq!(
             *invoker.target,
             Actor {
-                x:1,
-                y:0,
-                dx:0,
-                dy:-1
+                name: "Tom",
+                x: 10,
+                y: 10,
+                dx: 0,
+                dy: -1
             }
         );
     }
